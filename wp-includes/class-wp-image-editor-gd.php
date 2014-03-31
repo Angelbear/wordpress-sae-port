5a93e7ae649b2e48735ab319834a1052a6521556
Yangyang Zhao <zhao.yangyang@dena.jp>
2014-03-31 16:41:10 +0900

    fix class-wp-image-editor-gd and login and add neccessary files

--- wp-includes/class-wp-image-editor-gd.php
+++ wp-includes/class-wp-image-editor-gd.php
@@ -376,10 +376,6 @@ class WP_Image_Editor_GD extends WP_Image_Editor {
 			return new WP_Error( 'image_save_error', __('Image Editor Save Failed') );
 		}
 
-		// Set correct file permissions
-		$stat = stat( dirname( $filename ) );
-		$perms = $stat['mode'] & 0000666; //same permissions as parent folder, strip off the executable bits
-		@ chmod( $filename, $perms );
 
 		/**
 		 * Filter the name of the saved image file.
